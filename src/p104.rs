f51099cc8df921cae5318d571b4c0b032f01a700
LanQiDongLai <i1901433058@163.com>
2024-02-14 18:16:15 +0800

    添加题目104

--- src/p104.rs
+++ src/p104.rs
@@ -1,25 +1,10 @@
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-impl TreeNode {
-    #[inline]
-    #[allow(dead_code)]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None
-        }
-    }
-}
 struct Solution;
+
+use super::structure::TreeNode;
 
 use std::cell::RefCell;
 use std::rc::Rc;
+
 impl Solution {
     pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         let root = root;
