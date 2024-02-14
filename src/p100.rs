010dfc533917d7faa055ba4aac89401b1ca9ba9a
LanQiDongLai <i1901433058@163.com>
2024-02-14 18:16:00 +0800

    添加题目100

--- src/p100.rs
+++ src/p100.rs
@@ -1,24 +1,11 @@
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None
-        }
-    }
-}
+
 struct Solution;
+
+use super::structure::TreeNode;
 
 use std::rc::Rc;
 use std::cell::RefCell;
+
 impl Solution {
     pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
         if p.is_none() && q.is_none(){
